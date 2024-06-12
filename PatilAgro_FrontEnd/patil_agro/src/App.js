c7fde6ffaa1f63b1b3db8e6e934b95971c57400b
SUMITpatil2021 <sumit.patil2021@gmail.com>
2024-06-12 19:56:28 +0530

    setting up project

---
+++ PatilAgro_FrontEnd/patil_agro/src/App.js
@@ -0,0 +1,17 @@
+/* eslint-disable jsx-a11y/alt-text */
+import logo from './logo.svg';
+import './App.css';
+import imgage from '../public/Patil_agro.png'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>WelCome to PatilAgro</h1> 
+        <img src="imgage"></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
