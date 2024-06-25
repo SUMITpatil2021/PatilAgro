67c4631e1222b47ed5cf67730b6ef76421203cfb
SUMITpatil2021 <sumit.patil2021@gmail.com>
2024-06-25 12:02:47 +0530

    backend connected with mySQL Db

---
+++ PatilAgroMart_Backend/PatilAgroMart/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PatilAgroMart.Data;
+using PatilAgroMart.Models;
+
+namespace PatilAgroMart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;    
+        public UsersController(AppDbContext appDbContext) { 
+        
+            _appDbContext = appDbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser(User user)
+        {
+            _appDbContext.Users.Add(user);
+             await  _appDbContext.SaveChangesAsync();
+
+            return Ok(user );
+        }
+    }
+}
---
+++ PatilAgroMart_Backend/PatilAgroMart/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatilAgroMart.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PatilAgroMart.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+            public DbSet<User> Users{ get;set; }
+    }
+}
+
